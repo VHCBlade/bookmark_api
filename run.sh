9fd9cd155bc5edda3019505ae1a31f902f304f32
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2023-05-02 18:44:53 +0800

    Fixed run.sh for server deployment

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
+flutter pub get
+
 if [ -f "server.pid" ]; then
   pid=$(cat server.pid)
 
   if ps -p $pid > /dev/null; then
+    echo "Found existing server. Stopping current server..."
     pgid=$(ps -o pgid= -p $pid)
     kill -TERM "-$pgid"
   fi
@@ -12,3 +15,5 @@ fi
 export PORT=10290
 dart bin/server.dart > normal.log 2> error.log &
 echo $! > server.pid
+
+echo "Successfully launched server on port $PORT"
